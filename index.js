42ebe7ec788a623fb5cd3b01dbbe9f9bb075cc19
Ajmir Raziqi <ajmir.ng3@gmail.com>
2023-02-27 17:00:34 +0200

    single database file made

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-import Express from "./libs/express/index.js";
+const { Collection } = require("./libs/database");
+(async () => {
+  const users = new Collection("Users");
+  // for (let index = 0; index < 10; index++) {
+  //   await users.create({
+  //     index,
+  //   });
+  // }
+
+  // await users.update((doc) => doc.id === "1677504024480x666", { name: "Ali" });
+  // await users.clear();
+  console.log(
+    await users.find({
+      where: (a) => a.index === 3,
+    })
+  );
+})();
